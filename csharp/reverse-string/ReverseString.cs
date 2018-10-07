839c981f45d97afab5fc6a496c4c0f1c525150f3
riccardo.lomazzi <riccardo.lomazzi@outlook.it>
2018-10-07 10:37:25 +0200

    Gigasecond and ReverseString for CSharp, HelloWorld and Leap for Javascript

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        if (input.Length == 0)
+            return "";
+        else
+        {
+            char [] inputChar = input.ToCharArray();
+            char [] reversedString = new char[input.Length];
+            for(int j=input.Length-1, i=0; j>=0; j--,i++)
+            {
+                Debug.WriteLine("j = " + j + " reversedString[j] = " + reversedString[j]);
+                reversedString[j] = inputChar[i];
+            }
+            return new string(reversedString);
+        }
+    }
+}
